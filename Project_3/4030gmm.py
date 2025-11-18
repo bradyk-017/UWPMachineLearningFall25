a6bfb71ef97a7f7820a0795d762460f77ce6dd5b
gar4az <zachgarza03@gmail.com>
2025-11-18 09:07:41 -0600

    Updated the DET curve to plot over multiple thresholds

--- Project_3/4030gmm.py
+++ Project_3/4030gmm.py
@@ -336,8 +336,7 @@ def sample_log_likelihood(x, mean_matrix, cov_matrix, cw_matrix):
     return np.log(total_pdf)
 
 
-def classify_with_likelihood_ratio(test_set, gmm0, gmm1):
-    threshold = 0
+def classify_with_likelihood_ratio(test_set, gmm0, gmm1, threshold):
     mean0, cov0, w0 = gmm0
     mean1, cov1, w1 = gmm1
 
@@ -396,6 +395,7 @@ def multivariate_gaussian(pos, mu, Sigma):
 # Zach
 def EM_uwplatt_contour_plot(mean_matrix, cov_matrix, cw_matrix, label):
     no_components = cw_matrix.size
+    print("Components", no_components)
     no_GMMS = cw_matrix.shape[0]
     no_clusters = no_components // no_GMMS
     no_dim = 2
@@ -463,22 +463,41 @@ def EM_uwplatt_3D_plot(mean_matrix, cov_matrix, cw_matrix, label):
 # TODO
 
 # Ashton
-def det_curve(confusion_matrix):
-    TP = confusion_matrix[0, 0]
-    FN = confusion_matrix[0, 1]
-    FP = confusion_matrix[1, 0]
-    TN = confusion_matrix[1, 1]
+def det_curve(test_set, gmm0, gmm1):
+    num_of_points = 50
+    y_true = test_set[:, -1].astype(int)
+    X = test_set[:, :-1]
+    y_pred = []
+
+    mean0, cov0, w0 = gmm0
+    mean1, cov1, w1 = gmm1
+
+    lambdas = []
+    for x in X:
+        ll0 = sample_log_likelihood(x, mean0, cov0, w0)
+        ll1 = sample_log_likelihood(x, mean1, cov1, w1)
+        lambdas.append(ll1 - ll0)
+    lambdas = np.array(lambdas)
+
+    thresholds = np.linspace(np.min(lambdas), np.max(lambdas), num_of_points)
+
     FARs = []
     FRRs = []
-    FAR = FP / (FP + TN)
-    FRR = FN / (FN + TP)
-    FARs.append(FAR)
-    FRRs.append(FRR)
+
+    for thresh in thresholds:
+        cm, _ = classify_with_likelihood_ratio(test_set, gmm0, gmm1, thresh)
+        TP, FN = cm[0]
+        FP, TN = cm[1]
+        FAR = FP / (FP + TN)
+        FRR = FN / (FN + TP)
+        FARs.append(FAR)
+        FRRs.append(FRR)
+
+
+
     #all plt configuration within this function was made with a LLM
     plt.figure(figsize=(7, 6))
     plt.plot(FARs, FRRs, marker='o', linestyle='-')
-    plt.xscale('log')
-    plt.yscale('log')
     plt.xlabel("False Acceptance Rate (FAR)")
     plt.ylabel("False Rejection Rate (FRR)")
     plt.title("DET Curve")
@@ -567,12 +586,7 @@ def main():
     gmm0 = (mean0, cov0, w0)
     gmm1 = (mean1, cov1, w1)
 
-    confusion_matrix, accuracy = classify_with_likelihood_ratio(test_set, gmm0, gmm1)
-
-    print(confusion_matrix)
-    print(accuracy)
-
-    det_curve(confusion_matrix)
+    det_curve(test_set, gmm0, gmm1)
 
     # print(iterations)
     # print(dataset_log_likelihood_matrix)
