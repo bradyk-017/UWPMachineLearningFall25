d236818f98aa586e6d1afb987f803532eedb1009
gar4az <zachgarza03@gmail.com>
2025-12-11 16:06:22 -0600

    Moved classes to separate files

---
+++ Final_Project/TwoLayerNeuralNetwork.py
@@ -0,0 +1,169 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# A neural network with:
+INPUTS = 784
+HIDDEN = 40
+OUTPUTS = 10
+SAMPLES_USED = 2100
+
+learn_rate = 0.12
+max_epoch = 100
+
+# Gets a value and calculates the sigmoid activation function
+# Returns this calculated value
+def sigmoid(x):
+    # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
+# Gets a value and calculates to the derivative of sigmoid
+# Returns the calculated value
+def deriv_sigmoid(x):
+    # Derivative of sigmoid: f'(x) = f(x) * (1 - f(x))
+    fx = sigmoid(x)
+    return fx * (1 - fx)
+
+def softmax(z):
+    exps = np.exp(z - np.max(z))  # stability trick
+    return exps / np.sum(exps)
+
+# This function takes in np arrays of the same length and calculates
+# the square error for each sample and then finds the mean from those which is
+# the mean squared error, which is then returned
+def mse_loss(y_true, y_pred):
+    # y_true and y_pred are numpy arrays of the same length.
+    return ((y_true - y_pred) ** 2).mean()
+
+# Moving average difference function
+# num_elements to use with np arrays: considers elements to be only num_elements long
+def moving_avg_diff(elements, window_size, num_elements):
+    if num_elements < 12:
+        # Simply return average if less elements than the window size
+        return sum(elements)/num_elements
+    else:
+        diff = 0
+        # Calculate moving sum of differences
+        for i in range(1, window_size):
+            diff += abs(elements[num_elements - i] - elements[num_elements - i + 1])
+
+        # Return average of 
+        return diff / (window_size - 1)
+
+class TwoLayerNeuralNetwork:
+    # This code is an edit of the other nueral network in the code.
+    # It has been updated to use two hidden layers.
+    def __init__(self, hidden, learning_rate):
+        self.hidden = hidden
+
+        # Weights
+        self.weights1 = np.random.normal(size=(self.hidden, INPUTS))
+        self.weights2 = np.random.normal(size=(self.hidden, self.hidden))
+        self.weights3 = np.random.normal(size=(OUTPUTS, self.hidden))
+
+        # Biases
+        self.bias1 = np.random.normal(size=(self.hidden, 1))
+        self.bias2 = np.random.normal(size=(self.hidden, 1))
+        self.bias3 = np.random.normal(size=(OUTPUTS, 1))
+
+        self.learning_rate = learning_rate
+
+    def feedforward(self, x):
+        # x is a numpy array with 2 elements.
+        x = x.reshape(-1, 1)
+
+        # Hidden layer: h = sigmoid
+        z1 = self.weights1.dot(x) + self.bias1  # (4×784 @ 784×1) → (4×1)
+        h1 = sigmoid(z1)
+
+        z2 = self.weights2.dot(h1) + self.bias2
+        h2 = sigmoid(z2)
+
+        # Output layer: z = W2*h + b2
+        z3 = self.weights3.dot(h2) + self.bias3  # (10×4 @ 4×1) → (10×1)
+        o = softmax(z3)
+        return o.flatten()
+
+    def train(self, data, all_y_trues):
+        # Split the training set into actual train and cross-validation sets
+        data_train, data_cross_valid, y_train, y_cross_valid = train_test_split(
+            data, all_y_trues, test_size=0.2, random_state=42
+        )
+
+        mse_losses_training = np.zeros(max_epoch)
+        mse_losses_cv = np.zeros(max_epoch)
+
+        epoch = 0
+        stop = False
+        # Continue iterating while the moving average of MSE loss is
+        # above the threshold and not hit max epochs
+        while not stop and epoch < max_epoch:
+            for x, y_true in zip(data_train, y_train):
+                # 1. Ensure column vectors
+                x = x.reshape(-1, 1)  # (784,1)
+                y_true = y_true.reshape(-1, 1)  # (10,1)
+
+                # --- Do a feedforward (we'll need these values later)
+                # Hidden layer: h = sigmoid
+                z1 = self.weights1.dot(x) + self.bias1  # (4×784 @ 784×1) → (4×1)
+                h1 = sigmoid(z1)
+
+                z2 = self.weights2.dot(h1) + self.bias2
+                h2 = sigmoid(z2)
+
+                # Output layer: z = W2*h + b2
+                z3 = self.weights3.dot(h2) + self.bias3  # (10×4 @ 4×1) → (10×1)
+                o = softmax(z3)
+                y_pred = o
+                y_pred = y_pred.reshape(-1, 1)  # (10,1)
+                # --- Calculate partial derivatives.
+                # --- Naming: d_L_d_w1 represents "partial L / partial w1"
+
+                # Output layer gradient
+                dL_dy = 2 * (y_pred - y_true)  # (10,1)
+                dL_dw3 = dL_dy.dot(h2.T)  # (output_size, hidden_size) = (10,4)
+                dL_db3 = dL_dy  # (10,1)
+                dL_dh2 = self.weights3.T.dot(dL_dy)  # (hidden_size,1) = (4,1)
+
+                # 2nd Hidden layer gradient
+                dL_dz2 = dL_dh2 * deriv_sigmoid(z2)
+                dL_dw2 = dL_dz2.dot(h1.T)
+                dL_db2 = dL_dz2
+                dL_dh1 = self.weights2.T.dot(dL_dz2)
+
+                # 1st Hidden layer gradient
+                dL_dz1 = dL_dh1 * deriv_sigmoid(z1)
+                dL_dw1 = dL_dz1.dot(x.T)
+                dL_db1 = dL_dz1
+
+                # --- Update weights and biases
+                # Output layer
+                self.weights3 -= self.learning_rate * dL_dw3
+                self.bias3 -= self.learning_rate * dL_db3
+
+                # 2nd Hidden layer
+                self.weights2 -= self.learning_rate * dL_dw2
+                self.bias2 -= self.learning_rate * dL_db2
+
+                # 1st Hidden layer
+                self.weights1 -= self.learning_rate * dL_dw1
+                self.bias1 -= self.learning_rate * dL_db1
+
+            # Feedforward pass on training data set -> MSE loss on training data
+            y_preds = np.apply_along_axis(self.feedforward, 1, data_train)
+            mse_losses_training[epoch] = mse_loss(y_train, y_preds)
+
+            # Feedforward pass on CV set -> MSE loss on CV set
+            y_preds_cross_valid = np.apply_along_axis(self.feedforward, 1, data_cross_valid)
+            mse_losses_cv[epoch] = mse_loss(y_cross_valid, y_preds_cross_valid)
+
+            # Changed because there was really no change after 10 epochs
+            y_preds = np.apply_along_axis(self.feedforward, 1, data_train)
+            loss = mse_loss(y_train, y_preds)
+            print("Epoch %d loss: %.3f" % (epoch, loss))
+
+            # Stop condition
+            if epoch >= 12:
+                stop = moving_avg_diff(mse_losses_cv, 5, epoch) < 0.0015
+
+            epoch += 1
+        return mse_losses_training[:epoch], mse_losses_cv[:epoch]
